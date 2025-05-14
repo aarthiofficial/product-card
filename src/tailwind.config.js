e0052e36670a2829c4d3e8c929b76e10533b4483
aarthiofficial <aarthi.nbs@gmail.com>
2025-05-14 13:41:28 +0530

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
+/// FILE: src/index.css
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  @apply transition-colors duration-300;
+}
